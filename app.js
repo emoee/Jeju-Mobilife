7bd44cf10bb3828aebbcd9468e264305a3bc7e68
minsun <elinnane0@gmail.com>
2023-06-26 03:24:38 +0900

    node js test3

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 
 app.listen(8080, function(){
+    console.log((new Date()).toLocaleString());
     console.log("Listening on 8080");
 });
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/home.html");
+app.listen(3000, function(){
+    console.log((new Date()).toLocaleString());
+    console.log("App is running on port 3000");
 });
 
-app.listen(3000, function(){
-    console.log("App is running on port 3000");
+app.get("/", function(req, res){
+    console.log("req.ip => " + req.ip);
+    console.log("req.hostname => " + req.hostname);
+    res.sendFile(__dirname + "./home.html");
+});
+
+app.get("/area", function(req, res){
+    res.sendFile(__dirname + "./area/area.html");
 });
