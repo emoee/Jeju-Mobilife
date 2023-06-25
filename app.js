8c607075901a8e4efc07cf662600da8311e83b70
minsun <elinnane0@gmail.com>
2023-06-26 03:36:16 +0900

    node.js success

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ app.listen(8080, function(){
     console.log("Listening on 8080");
 });
 
-app.listen(3000, function(){
-    console.log((new Date()).toLocaleString());
-    console.log("App is running on port 3000");
-});
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + "/public"));
 
+// Define routes
 app.get("/", function(req, res){
     console.log("req.ip => " + req.ip);
     console.log("req.hostname => " + req.hostname);
     res.sendFile(__dirname + "/home.html");
 });
 
-app.get("/area", function(req, res){
-    res.sendFile(__dirname + "/area/area.html");
+// Start the server
+app.listen(3000, function(){
+    console.log("App is running on port 3000");
 });
