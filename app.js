35deeed2c586490bb238309a93cdb0ff24c8a35d
minsun <elinnane0@gmail.com>
2023-06-26 03:50:41 +0900

    node.js ..plz

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ app.listen(8080, function(){
     console.log("Listening on 8080");
 });
 
-// Serve static files from the "public" directory
-app.use(express.static(__dirname + "/image"));
-app.use(express.static(__dirname + "/area"));
+// Serve static files from the "image" directory
+app.use('/image', express.static(__dirname + '/image'));
+
+// Serve static files from the "area" directory
+app.use('/area', express.static(__dirname + '/area'));
+
 // Define routes
 app.get("/", function(req, res){
     console.log("req.ip => " + req.ip);
